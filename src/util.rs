320d6179aad87c1ec38ea2d9a2531fab6244b968
Austin <ahaskell@carthage.edu>
2021-08-25 18:39:27 -0500

    Added function to map byte to human readable instruction name

--- src/util.rs
+++ src/util.rs
@@ -213,6 +213,124 @@ pub fn map_instruction_to_addressing_mode(instruction: u8) -> AddressingMode {
     }
 }
 
+pub fn map_instruction_to_name(instruction: u8) -> &str {
+    match instruction {
+        // ADC
+        0x69 | 0x65 | 0x75 | 0x6D | 0x7D | 0x79 | 0x61 | 0x71 => "ADC",
+        // AND
+        0x29 | 0x25 | 0x35 | 0x2D | 0x3D | 0x39 | 0x21 | 0x31 => "AND",
+        // ASL
+        0x0A | 0x06 | 0x16 | 0x0E | 0x1E => "ASL",
+        // BCC
+        0x90 => "BCC",
+        // BCS
+        0xB0 => "BCS",
+        // BEQ
+        0xF0 => "BEQ",
+        // BIT
+        0x24 | 0x2C => "BIT",
+        // BMI
+        0x30 => "BMI",
+        // BNE
+        0xD0 => "BNE",
+        // BPL
+        0x10 => "BPL",
+        // BRK
+        0x00 => "BRK",
+        // BVC
+        0x50 => "BVC",
+        // BVS
+        0x70 => "BVS",
+        // CLC
+        0x18 => "CLC",
+        // CLD
+        0xD8 => "CLD",
+        // CLI
+        0x58 => "CLI",
+        // CLV
+        0xB8 => "CLV",
+        // CMP
+        0xC9 | 0xC5 | 0xD5 | 0xCD | 0xDD | 0xD9 | 0xC1 | 0xD1 => "CMP",
+        // CPX
+        0xE0 | 0xE4 | 0xEC => "CPX",
+        // CPY
+        0xC0 | 0xC4 | 0xCC => "CPY",
+        // DEC
+        0xC6 | 0xD6 | 0xCE | 0xDE => "DEC",
+        // DEX
+        0xCA => "DEX",
+        // DEY
+        0x88 => "DEY",
+        // EOR
+        0x49 | 0x45 | 0x55 | 0x4D | 0x5D | 0x59 | 0x41 | 0x51 => "EOR",
+        // INC
+        0xE6 | 0xF6 | 0xEE | 0xFE => "INC",
+        // INX
+        0xE8 => "INX",
+        // INY
+        0xC8 => "INY",
+        // JMP
+        0x4C | 0x6C => "JMP",
+        // JSR
+        0x20 => "JSR",
+        // LDA
+        0xA9 | 0xA5 | 0xB5 | 0xAD | 0xBD | 0xB9 | 0xA1 | 0xB1 => "LDA",
+        // LDX
+        0xA2 | 0xA6 | 0xB6 | 0xAE | 0xBE => "LDX",
+        // LDY
+        0xA0 | 0xA4 | 0xB4 | 0xAC | 0xBC => "LDY",
+        // LSR
+        0x4A | 0x46 | 0x56 | 0x4E | 0x5E => "LSR",
+        // NOP
+        0xEA => "NOP",
+        // ORA
+        0x09 | 0x05 | 0x15 | 0x0D | 0x1D | 0x19 | 0x01 | 0x11 => "ORA",
+        // PHA
+        0x48 => "PHA",
+        // PHP
+        0x08 => "PHP",
+        // PLA
+        0x68 => "PLA",
+        // PLP
+        0x28 => "PLP",
+        // ROL
+        0x2A | 0x26 | 0x36 | 0x2E | 0x3E => "ROL",
+        // ROR
+        0x6A | 0x66 | 0x76 | 0x6E | 0x7E => "ROR",
+        // RTI
+        0x40 => "RTI",
+        // RTS
+        0x60 => "RTS",
+        // SBC
+        0xE9 | 0xE5 | 0xF5 | 0xED | 0xFD | 0xF9 | 0xE1 | 0xF1 => "SBC",
+        // SEC
+        0x38 => "SEC",
+        // SED
+        0xF8 => "SED",
+        // SEI
+        0x78 => "SEI",
+        // STA
+        0x85 | 0x95 | 0x8D | 0x9D | 0x99 | 0x81 | 0x91 => "STA",
+        // STX
+        0x86 | 0x96 | 0x8E => "STX",
+        // STY
+        0x84 | 0x94 | 0x8C => "STY",
+        // TAX
+        0xAA => "TAX",
+        // TAY
+        0xA8 => "TAY",
+        // TSX
+        0xBA => "TSX",
+        // TXA
+        0x8A => "TXA",
+        // TXS
+        0x9A => "TXS",
+        // TYA
+        0x98 => "TYA",
+        _ => panic!("Got unrecognized instruction while mapping addressing mode. ")
+    }
+}
+
 pub fn addressing_mode_to_program_counter_advancement_amount(mode: AddressingMode) -> u16 {
     match mode {
         AddressingMode::Implied => 1,
